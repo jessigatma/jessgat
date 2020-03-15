a28a91d1ae7539334847683a845f64b2c3d22746
jessigatma <jessgat@hotmail.com>
2020-03-15 16:04:02 -0600

    Updates

---
+++ precache-manifest.a3d2e791bd255a6f6e7443aa113fc685.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32fd1df661fe6558d92d1e87f7033e77",
+    "url": "/portafolio-jessgat/index.html"
+  },
+  {
+    "revision": "643249a5c1879faa7c20",
+    "url": "/portafolio-jessgat/static/css/main.9bd8e87a.chunk.css"
+  },
+  {
+    "revision": "828af208a05d9d8e1cd6",
+    "url": "/portafolio-jessgat/static/js/2.8071d047.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/portafolio-jessgat/static/js/2.8071d047.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "643249a5c1879faa7c20",
+    "url": "/portafolio-jessgat/static/js/main.e62e3690.chunk.js"
+  },
+  {
+    "revision": "f01205780e8eef9d5077",
+    "url": "/portafolio-jessgat/static/js/runtime-main.d0db8b87.js"
+  },
+  {
+    "revision": "0c852f204d0b6c88d5ebfccdae1daf16",
+    "url": "/portafolio-jessgat/static/media/blackholes.0c852f20.png"
+  }
+]);
