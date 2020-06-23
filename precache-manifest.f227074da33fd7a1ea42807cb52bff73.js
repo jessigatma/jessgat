2390bd7983ba5300fd059b1c961795a3558ca62d
jessigatma <jessgat@hotmail.com>
2020-06-22 23:33:58 -0500

    Updates

--- precache-manifest.f227074da33fd7a1ea42807cb52bff73.js
+++ precache-manifest.f227074da33fd7a1ea42807cb52bff73.js
@@ -1,27 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "2ddd77693a89b4dfd0594a29bc3654f1",
+    "revision": "1494144c24316fa9774a923d32468e6a",
     "url": "/portafolio-jessgat/index.html"
   },
   {
-    "revision": "21bd715e1c30a1b4423a",
-    "url": "/portafolio-jessgat/static/css/2.de424728.chunk.css"
-  },
-  {
-    "revision": "59e15c6952140cfb9e52",
+    "revision": "b6ad7d15e3abf6247505",
     "url": "/portafolio-jessgat/static/css/main.9bd8e87a.chunk.css"
   },
   {
-    "revision": "21bd715e1c30a1b4423a",
-    "url": "/portafolio-jessgat/static/js/2.7441d900.chunk.js"
+    "revision": "828af208a05d9d8e1cd6",
+    "url": "/portafolio-jessgat/static/js/2.8071d047.chunk.js"
   },
   {
     "revision": "11a928b57aca5049fe950bf8bcde77c5",
-    "url": "/portafolio-jessgat/static/js/2.7441d900.chunk.js.LICENSE.txt"
+    "url": "/portafolio-jessgat/static/js/2.8071d047.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "59e15c6952140cfb9e52",
-    "url": "/portafolio-jessgat/static/js/main.e1805252.chunk.js"
+    "revision": "b6ad7d15e3abf6247505",
+    "url": "/portafolio-jessgat/static/js/main.242e68de.chunk.js"
   },
   {
     "revision": "f01205780e8eef9d5077",
